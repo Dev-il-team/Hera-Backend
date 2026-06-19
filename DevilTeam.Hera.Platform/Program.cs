04370405b2f82c5116aa6eae4d4aef42d99091ae
Piero Molina <molinna.leo@gmail.com>
2026-06-19 20:42:30 +0200

    feat(devices-management): register dependencies and application configuration

--- DevilTeam.Hera.Platform/Program.cs
+++ DevilTeam.Hera.Platform/Program.cs
@@ -1,19 +1,122 @@
+using DevilTeam.Hera.Platform.DevicesManagement.Application.CommandServices;
+using DevilTeam.Hera.Platform.DevicesManagement.Application.Internal.CommandServices;
+using DevilTeam.Hera.Platform.DevicesManagement.Application.Internal.QueryServices;
+using DevilTeam.Hera.Platform.DevicesManagement.Application.QueryServices;
+using DevilTeam.Hera.Platform.DevicesManagement.Domain.Repositories;
+using DevilTeam.Hera.Platform.DevicesManagement.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+using DevilTeam.Hera.Platform.Resources.Errors;
+using DevilTeam.Hera.Platform.Resources.Shared;
+using DevilTeam.Hera.Platform.Shared.Domain.Repositories;
+using DevilTeam.Hera.Platform.Shared.Infrastructure.Interfaces.AspNetCore.Configuration;
+using DevilTeam.Hera.Platform.Shared.Infrastructure.Mediator.Cortex.Configuration;
+using DevilTeam.Hera.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+using DevilTeam.Hera.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+using DevilTeam.Hera.Platform.Shared.Infrastructure.Pipeline.Middleware.Extensions;
+using Cortex.Mediator.Commands;
+using Cortex.Mediator.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using Microsoft.OpenApi;
+using ProblemDetailsFactory = DevilTeam.Hera.Platform.Shared.Interfaces.Rest.ProblemDetails.ProblemDetailsFactory;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()))
+    .AddDataAnnotationsLocalization();
+
+// Add ProblemDetails services
+builder.Services.AddProblemDetails();
+
+// Add CORS Policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllPolicy",
+        policy => policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+// Add Database Connection
+builder.Services.AddDbContext<AppDbContext>((serviceProvider, options) =>
+{
+    var connectionStringTemplate = builder.Configuration.GetConnectionString("DefaultConnection");
+    if (string.IsNullOrWhiteSpace(connectionStringTemplate))
+        throw new InvalidOperationException("Database connection string is not set in the configuration.");
+
+    var connectionString = Environment.ExpandEnvironmentVariables(connectionStringTemplate);
+    if (string.IsNullOrWhiteSpace(connectionString))
+        throw new InvalidOperationException("Database connection string is not set in the configuration.");
+
+    options.UseMySQL(connectionString)
+        .UseLoggerFactory(serviceProvider.GetRequiredService<ILoggerFactory>())
+        .EnableDetailedErrors();
+
+    if (builder.Environment.IsDevelopment())
+        options.EnableSensitiveDataLogging();
+});
+
+// Localization
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddSingleton<IStringLocalizer<ErrorMessages>, StringLocalizer<ErrorMessages>>();
+builder.Services.AddSingleton<IStringLocalizer<CommonMessages>, StringLocalizer<CommonMessages>>();
+
+// Register the custom ProblemDetailsFactory
+builder.Services.AddSingleton<ProblemDetailsFactory>();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.EnableAnnotations();
+});
+
+// Dependency Injection
+
+// Shared Bounded Context
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// DevicesManagement Bounded Context
+builder.Services.AddScoped<IDeviceRepository, DeviceRepository>();
+builder.Services.AddScoped<IDeviceCommandService, DeviceCommandService>();
+builder.Services.AddScoped<IDeviceQueryService, DeviceQueryService>();
+
+// Mediator Configuration
+builder.Services.AddScoped(typeof(ICommandPipelineBehavior<>), typeof(LoggingCommandBehavior<>));
+
+builder.Services.AddCortexMediator(
+    [typeof(Program)]);
 
 var app = builder.Build();
 
+// Apply pending migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
+app.UseGlobalExceptionHandler();
+
+var supportedCultures = new[] { "en", "es" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Apply CORS Policy
+app.UseCors("AllowAllPolicy");
 
 app.UseHttpsRedirection();
 
